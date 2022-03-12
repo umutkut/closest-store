5b9f1913670536ac33699bc3279ab31b54cc7d4b
umutku <umututku.tanis@gmail.com>
2022-03-12 16:41:50 +0300

    Initial commit

---
+++ src/main/java/com/jumbo/closeststore/ClosestStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jumbo.closeststore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosestStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClosestStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jumbo/closeststore/ClosestStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumbo.closeststore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosestStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
